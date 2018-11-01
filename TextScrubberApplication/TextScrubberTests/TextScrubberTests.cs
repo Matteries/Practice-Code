9c38345c2248c5600195241c9cdf4f732b121ae8
matthew-price-microlise <41467301+matthew-price-microlise@users.noreply.github.com>
2018-11-01 14:47:20 +0000

    Updated TextScrubberTests with 60% coverage

--- TextScrubberApplication/TextScrubberTests/TextScrubberTests.cs
+++ TextScrubberApplication/TextScrubberTests/TextScrubberTests.cs
@@ -13,5 +13,16 @@ namespace TextScrubberTests
         {
             _sut = new TextScrubberLogic();
         }
+
+        [Test]
+        [TestCase("<script>Hello, World!</script>", "Hello, World!")]
+        [TestCase("<p>This is a test</p>", "This is a test")]
+        [TestCase("<head>This is a header</head>", "This is a header")]
+        [TestCase("<html>This is a html code snippet</html>", "This is a html code snippet")]
+        [TestCase("<body>This is some body HTML code</body>", "This is some body HTML code")]
+        public void GivenOutput_DoesntContainAnyHTMLTags(string testInput, string expectedOutput)
+        {
+            Assert.That(expectedOutput, Is.EqualTo(_sut.StripHTMLTags(testInput)));
+        }
     }
 }
