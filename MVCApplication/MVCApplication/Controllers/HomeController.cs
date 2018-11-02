c87b0f658964dab392f92716966db549db9ada5b
matthew-price-microlise <41467301+matthew-price-microlise@users.noreply.github.com>
2018-11-02 15:42:54 +0000

    1st try at an MVCApplication, with unit tests

---
+++ MVCApplication/MVCApplication/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCApplication.Models;
+
+namespace MVCApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult About()
+        {
+            ViewBag.Message = "MVC About";
+
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            ViewBag.Message = "MVC Index";
+            var reader = new StreamReader(System.IO.File.OpenRead(@"E:\MVCDATA.csv"));
+            var users = new List<UserInfo>();
+
+            bool notFirstLine = true;
+
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                if (notFirstLine)
+                {
+                    notFirstLine = false;
+                }
+                else
+                {
+                    if (!string.IsNullOrWhiteSpace(line))
+                    {
+                        var values = line.Split(',');
+                        if (values.Length >= 4)
+                        {
+                            users.Add(new UserInfo
+                                {ID = int.Parse(values[0]), Name = values[1], Email = values[2], Number = values[3]});
+                        }
+                    }
+                }
+            }
+
+            return View(users);
+        }
+    }
+}
---
+++ MVCApplication/MVCApplicationTests.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,68 @@
+﻿using MVCApplication.Controllers;
+using NUnit.Framework;
+using System.Web.Mvc;
+using Assert = NUnit.Framework.Assert;
+
+namespace MVCApplicationTests.Tests.Controllers
+{
+    [TestFixture]
+    public class HomeControllerTest
+    {
+        private HomeController _sut;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            // Arrange
+            _sut = new HomeController();
+        }
+
+        [Test]
+        public void Index()
+        {
+
+            // Act
+            ViewResult result = _sut.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void IndexReturnsAValue()
+        {
+
+            // Act
+            ViewResult result = _sut.Index() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("MVC Index", result.ViewBag.Message);
+        }
+
+        [Test]
+        [TestCase("Allan Cano", "Allan Cano")]
+        [TestCase("a.cano@outlook.co.uk", "a.cano@outlook.co.uk")]
+        [TestCase("07779 678735", "07779 678735")]
+        public void IndexReturnsStudentData(int testInput, bool expectedOutput)
+        {
+
+            // Act
+            var indexTableResults = _sut.Index(testInput);
+
+            // Assert
+            Assert.That(expectedOutput, Is.EqualTo(indexTableResults));
+        }
+
+        [Test]
+        public void AboutReturnsAValue()
+        {
+
+            // Act
+            ViewResult result = _sut.About() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("Text MVC", result.ViewBag.Message);
+        }
+    }
+}
