09543e274c7b31a88168eddee906733bed034bad
matthew-price-microlise <41467301+matthew-price-microlise@users.noreply.github.com>
2018-11-27 11:53:01 +0000

    First attempt at a bowling kata, making unit tests as quickly as i could.

---
+++ BowlingGame/BowlingGame/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace BowlingGame
+{
+    public class Game
+    {
+        int[] pinFalls = new int[21];
+        int rollCounter;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bowling Kata!");
+        }
+
+        public void Roll(int pins)
+        {
+            pinFalls[rollCounter] = pins;
+            rollCounter++;
+        }
+
+        public int Score()
+        {
+            int i = 0;
+            int score = 0;
+
+            score = Frames(i, score);
+            return score;
+        }
+
+        private int Frames(int i, int score)
+        {
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (pinFalls[i] + pinFalls[i + 1] == 10)
+                {
+                    score += 10 + pinFalls[i + 2];
+                    i += 2;
+                }
+                else
+                {
+                    score += pinFalls[i] + pinFalls[i + 1];
+                    i += 1;
+                }
+            }
+
+            return score;
+        }
+    }
+}
---
+++ BowlingGame/BowlingGameTests/UnitTest1.cs
@@ -0,0 +1,65 @@
+using BowlingGame;
+using NUnit.Framework;
+
+namespace BowlingGameTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        private Game _sut;
+
+        [SetUp]
+        public void Create_NewInstanceOf_Game()
+        {
+            _sut = new Game();
+        }
+
+        void AmountOf_RollsIn_ASingleTurn(int rolls, int pins)
+        {
+            for (int i = 0; i < rolls; i++)
+            {
+                _sut.Roll(pins);
+            }
+        }
+
+        [Test]
+        public void Create_NewInstanceOfA_RollInGame()
+        {
+            AmountOf_RollsIn_ASingleTurn(20, 0);
+            Assert.That(_sut.Score(), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Create_NewInstanceOfA_SingleRoll()
+        {
+            AmountOf_RollsIn_ASingleTurn(20, 1);
+            Assert.That(_sut.Score(), Is.EqualTo(20));
+        }
+
+        [Test]
+        public void InstanceOf_RollingASpareOnFirstFrame()
+        {
+            _sut.Roll(9);
+            _sut.Roll(1);
+            AmountOf_RollsIn_ASingleTurn(18, 1);
+
+            Assert.That(_sut.Score(), Is.EqualTo(29));
+        }
+
+        [Test]
+        public void RollSparesEveryFrame()
+        {
+            AmountOf_RollsIn_ASingleTurn(21, 5);
+
+            Assert.That(_sut.Score(), Is.EqualTo(150));
+        }
+
+        [Test]
+        public void RollPerfectGame()
+        {
+            AmountOf_RollsIn_ASingleTurn(12, 10)
+
+            Assert.That(_sut.Score(), Is.EqualTo(300));
+        }
+    }
+}
