c265e75b7ce9aaf7df2531010c728b9229ef4ae2
Matthew Price <matthew.price46@outlook.com>
2020-02-20 15:39:19 +0000

    MVCApplication, tests and sample data.

---
+++ MVCApplication/MVCApplication/Models/UserInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCApplication.Models
+{
+    public class UserInfo
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Number { get; set; }
+    }
+}
